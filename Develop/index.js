d688ea81d6a8e46972f2e5aa380665e6204c44c1
Selina Su <phongsu20@gmail.com>
2022-10-16 19:21:20 +0700

    ready to run testing

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+const markdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    }, 
+    {
+        type: "input",
+        message: "Please enter a short description of this project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is your project installation instructions?",
+        name: "instruction"
+    },
+    {
+        type: "input",
+        message: "What is your project usage information?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What is your project contributing?",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "What is your project testing procedure?",
+        name: "testing"
+    },
+    {
+        type: "list",
+        message: "What license are you using for the project?",
+        name: "license",
+        choices:[
+            'MIT',
+            'ISC',
+            'Apache'
+        ]
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, markdown(data), (err) => 
+       err ? console.log(err) : console.log('Success!')  
+    );
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(res => {
+        console.log(res);
+        writeToFile(`${res.title}.md`, res);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,143 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+var userName = '';
+// TODO: Create a function that returns a license badge and link based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'; 
+    case 'ISC':
+      return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+    case 'Apache':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch(license) {
+    case 'MIT':
+      return `Copyright (c) 2022 ${userName}
+
+      Permission is hereby granted, free of charge, to any person obtaining
+      a copy of this software and associated documentation files (the
+      "Software"), to deal in the Software without restriction, including
+      without limitation the rights to use, copy, modify, merge, publish,
+      distribute, sublicense, and/or sell copies of the Software, and to
+      permit persons to whom the Software is furnished to do so, subject to
+      the following conditions:
+      
+      The above copyright notice and this permission notice shall be
+      included in all copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+      NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+      LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+      OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+      WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+    case 'ISC':
+      return `Copyright (c) 2022 ${userName}
+
+      Permission to use, copy, modify, and/or distribute this software for any
+      purpose with or without fee is hereby granted, provided that the above
+      copyright notice and this permission notice appear in all copies.
+
+      THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+      WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+      MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+      ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+      WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+      ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+      OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`;
+    case 'Apache':
+      return `Copyright (c) 2022 ${userName}
+
+      Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.`;
+        default:
+          return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  userName = data.github;
+  let licenseBadge = renderLicenseBadge(data.license);
+  let licenseInfo = renderLicenseSection(data.license);
   return `# ${data.title}
+  ${licenseBadge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+  * [Instruction](#instruction)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Testing](#testing)
+  * [Questions](#questions)
+
+<a name="instruction"/>
+
+## Instruction
+
+Please follow the instruction to install the app
+
+${data.instruction}
+
+<a name="usage"/>
+
+## Usage
+
+${data.usage}
+
+<a name="license"/>
+
+## License
+
+${licenseInfo}
+
+<a name="contributing"/>
+
+## Contributing
+
+${data.contributing}
+
+<a name="testing"/>
+
+## Testing
+
+Please follow this command to run the test:
+
+${data.testing}
+
+<a name="questions"/>
+
+## Questions
+
+[Email me for more information]: 
+
+${data.email}
+
+[Check out my other projects on Github]
+
+Link: https://www.github.com/${userName}
 
 `;
 }
